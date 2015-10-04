48fed8ac1ba396da686a696e9e74368e3859bdeb
Anojh Gnanachandran <a3gnanac@uwaterloo.ca>
2015-10-04 02:43:16 -0400

    Set up thinky and rethinkdb dependencies. Set up some models. TODO: http://thinky.io/documentation/architecture/

--- drawing-game/app.js
+++ drawing-game/app.js
@@ -1,11 +1,13 @@
-// Including libraries
+var thinky = require('thinky')();
 var express = require('express');
-var app = express();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
+var http = require('http');
+var socketIo = require('socket.io');
 
-// This is the port for our web server.
-// you will need to go to http://localhost:8080 to see it
+// Setup server and Socket.io
+var app = express();
+var server = http.Server(app);
+var io = socketIo(server);
+
 server.listen(3000);
 
 app.use('/assets', express.static('assets'));
@@ -13,17 +15,40 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-// Delete this row if you want to see debug messages
-io.set('log level', 1);
-
 // Listen for incoming connections from clients
 io.sockets.on('connection', function (socket) {
 
-	// Start listening for mouse move events
-	socket.on('mousemove', function (data) {
-		
-		// This line sends the event (broadcasts it)
-		// to everyone except the originating client.
-		socket.broadcast.emit('moving', data);
-	});
+    // Start listening for mouse move events
+    socket.on('mousemove', function (data) {
+
+        // This line sends the event (broadcasts it)
+        // to everyone except the originating client.
+        socket.broadcast.emit('moving', data);
+    });
 });
+
+var type = thinky.type;
+var r = thinky.r;
+
+// Create models
+
+var Drawing = thinky.createModel('Drawing', {
+    id: type.string(),
+    createdAt: type.date().default(r.now())
+}); 
+
+var Point = thinky.createModel('Point', {
+    id: type.string(),
+    point: type.point(),
+    lineId: type.string(),
+    createdAt: type.date().default(r.now())
+});
+
+var Line = thinky.createModel('Line', {
+    id: type.string(),
+    createdAt: type.date().default(r.now()),
+    drawingId: type.string()
+}); 
+
+Line.hasMany(Point, 'points', 'id', 'lineId');
+Drawing.hasMany(Line, 'lines', 'id', 'drawingId');
