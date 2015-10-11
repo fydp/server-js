1863f7a4b7f4d7a577c9581836889bb2e3189805
Kevin Pan <xcpankevin@hotmail.com>
2015-10-10 23:09:45 -0400

    Moves drawing game stuff to the actual app

--- app.js
+++ app.js
@@ -1,13 +1,35 @@
+var body_parser  = require('body-parser');
 var express = require('express');
 var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
-app.get('/', function (req, res) {
-      res.send('Hello World!');
+server.listen(8080);
+
+app.set('view engine', 'ejs');
+app.use(express.static('.'));
+app.use(body_parser.urlencoded({ extended: false }));
+app.use(body_parser.json());
+
+app.get('/test', function(request, response) {
+    response.render('test');
 });
 
-var server = app.listen(8080, function () {
-      var host = server.address().address;
-        var port = server.address().port;
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
 
-          console.log('Example app listening at http://%s:%s', host, port);
+// Delete this row if you want to see debug messages
+io.set('log level', 1);
+
+// Listen for incoming connections from clients
+io.sockets.on('connection', function (socket) {
+
+    // Start listening for mouse move events
+    socket.on('mousemove', function (data) {
+        
+        // This line sends the event (broadcasts it)
+        // to everyone except the originating client.
+        socket.broadcast.emit('moving', data);
+    });
 });
